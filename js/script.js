e8b390adf682be4d542295103ed375833f8ca83f
Enzo Wilkosz <wilkoszenzoyan@gmail.com>
2025-12-19 21:47:47 -0300

    Otimizações finais

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// Ativar items do menu
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)) {
+        link.classList.add('ativo');
+    }
+}
+
+links.forEach(ativarLink)
+
+// Ativar items do Orçamento
+
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+    
+}
+
+paramentros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');
+    pergunta.setAttribute('aria-expanded', true);
+    
+    
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+
+
+perguntas.forEach(eventosPerguntas);
+
+// Animation
+if (window.SimpleAnime) {
+    new SimpleAnime()
+}
+
+
+
